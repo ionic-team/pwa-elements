0be8f471ab6f3c8fb064bd1a8f40ad6591af9382
Max Lynch <max@drifty.com>
2018-02-06 15:25:18 -0600

    Camera

--- src/components/camera/camera.tsx
+++ src/components/camera/camera.tsx
@@ -6,10 +6,37 @@ import { Component, Prop } from '@stencil/core';
   shadow: true
 })
 export class Camera {
+  @Prop({ context: 'isServer' }) private isServer: boolean;
+
+  stream: MediaStream;
+  mediaRecorder: any;
+  videoElement: HTMLVideoElement;
+
+  async componentDidLoad() {
+    if (this.isServer) {
+      return;
+    }
+
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({
+        video: true,
+        audio: false
+      });
+
+      this.stream = stream;
+      this.videoElement.srcObject = stream;
+    } catch(e) {
+      console.error(e);
+    }
+  }
   render() {
-    return (
-      <div>
+    return [
+      <video ref={(el: HTMLVideoElement) => this.videoElement = el} autoplay></video>,
+      <div class="shutter">
+        <div class="shutter-ring">
+          <div class="shutter-button"></div>
+        </div>
       </div>
-    );
+    ];
   }
 }
