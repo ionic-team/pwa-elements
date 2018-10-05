7dc5740ccd8b69fa7c68c706acbe9e0bc6c8fed9
Max Lynch <max@drifty.com>
2018-10-05 15:52:00 -0500

    Working

--- stencil.config.js
+++ stencil.config.js
@@ -1,5 +1,3 @@
-const sass = require('@stencil/sass');
-
 exports.config = {
   namespace: 'ionicpwaelements',
   outputTargets: [
@@ -9,29 +7,5 @@ exports.config = {
     {
       type: 'www'
     }
-  ],
-  // generateDistribution: true,
-  // Depend on these ionic controls
-  /*
-  bundles: [
-    { components: ['ion-icon', 'ion-action-sheet-controller', 'ion-action-sheet',
-                   'ion-modal-controller', 'ion-modal', 'ion-animation-controller',
-                   'ion-toast-controller', 'ion-toast'] },
-    { components: ['ion-pwa-camera-modal', 'ion-pwa-camera', 'ion-pwa-action-sheet'] }
-  ],
-  collections: [
-    { 
-      name: '@ionic/core'
-    }
-  ],
-  */
-  plugins: [
-    // sass()
   ]
 };
-
-exports.devServer = {
-  root: 'www',
-  watchGlob: '**/**',
-  ssl: true
-}
